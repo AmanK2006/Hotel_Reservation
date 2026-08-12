f9673fa05869eeb31ca837b45f5e47035ae15352
AmanK2006 <amannk2006@gmail.com>
2026-08-13 03:44:05 +0530

    data visualization and preprocessing

--- config/paths_config.py
+++ config/paths_config.py
@@ -10,10 +10,21 @@ TEST_FILE_PATH = os.path.join(RAW_DIR, "test.csv")
 
 CONFIG_PATH = "config/config.yaml"
 
+############################### DATA PREPROCESSING ###########################
+
+PREPROCESSED_DIR = "artifacts/preprocessed"
+PREPROCESSED_TRAIN_PATH = os.path.join(PREPROCESSED_DIR, "train_preprocessed.csv")
+PREPROCESSED_TEST_PATH = os.path.join(PREPROCESSED_DIR, "test_preprocessed.csv")
+PREPROCESSOR_OBJ_PATH = os.path.join(PREPROCESSED_DIR, "preprocessor.pkl")
+
 __all__ = [
     "RAW_DIR",
     "RAW_FILE_PATH",
     "TRAIN_FILE_PATH",
     "TEST_FILE_PATH",
     "CONFIG_PATH",
+    "PREPROCESSED_DIR",
+    "PREPROCESSED_TRAIN_PATH",
+    "PREPROCESSED_TEST_PATH",
+    "PREPROCESSOR_OBJ_PATH",
 ]
